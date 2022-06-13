330abdaa5871e80f4da2240e781e1323eca0ee66
miros <miro@miro.miro>
2022-06-13 18:45:00 +0200

    zaciatok dabazy s hracmi

---
+++ app/src/main/java/sk/aplikacie/kviz/data/User.kt
@@ -0,0 +1,12 @@
+package sk.aplikacie.kviz.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val meno: String,
+    val skore: Int
+        )
